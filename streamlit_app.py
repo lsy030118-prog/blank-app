88ecfe75461f5865606d3e89e83d75a48eba765c
lsy030118-prog <lsy030118@gmail.com>
2026-03-16 06:48:35 +0000

    자기소개 페이지 추가 및 내용 업데이트

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("👋 자기소개")
+
+st.header("안녕하세요!")
+st.write("저는 이수연입니다. [202202140 이수연]")
+
+st.subheader("관심사")
+st.write("- [임용 공부]")
+st.write("- [오늘의 메뉴]")
+
+st.subheader("기술 및 스킬")
+st.write("- [수학교육]")
+st.write("- [정보 전달]")
+
+st.subheader("연락처")
+st.write("이메일: [lsy8045@naver.com]")
+st.write("GitHub: [https://github.com/lsy030118-prog]")
+
+st.write("더 자세한 정보는 위의 링크를 확인해주세요!")
